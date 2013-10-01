050f4cb0968b990648b05a6f0a640856abf7b212
Manu Sridharan <msridhar@us.ibm.com>
2013-10-01 09:28:14 -0700

    add license header

--- node-index-gen.js
+++ node-index-gen.js
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * @license
+ * Copyright (c) 2013 IBM Corporation.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 
+ * (http://www.eclipse.org/legal/epl-v10.html), and the Eclipse Distribution 
+ * License v1.0 (http://www.eclipse.org/org/documents/edl-v10.html). 
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 /*jslint node:true */
 
 var fs = require('fs'),
